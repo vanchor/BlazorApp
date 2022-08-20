0aed5086a24c553736c8ed2f4db06ec2691da3c9
vanchor <vovanyakorev2@gmail.com>
2022-08-20 20:11:32 +0200

    Replacing input with EditForm

--- BlazorApp1/Data/Grocery.cs
+++ BlazorApp1/Data/Grocery.cs
@@ -1,8 +1,16 @@
-﻿namespace BlazorApp1.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name should be less than 20 characters!")]
         public string? Name { get; set; }
+
+
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Valid Price Range is (1-10000)")]
         public float Price { get; set; }
     }
 }
